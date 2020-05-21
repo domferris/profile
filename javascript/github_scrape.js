7effd7e356106e3caf4f76caace565d0d4b4c4d0
Dominic Ferris <ferris.dominic@gmail.com>
2020-05-20 22:54:57 -0500

    move javascript into separate files

---
+++ javascript/github_scrape.js
@@ -0,0 +1,6 @@
+// CONTRIBUTIONS NUMBER RETRIEVAL VIA GITHUB SCRAPING
+// https://github.com/domferris
+// <h2 class="f4 text-normal mb-2">
+//   _#_ contributions
+//     in the last year
+// </h2>
