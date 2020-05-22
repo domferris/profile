8fa7523b97da6403f96058682374e20258da00e1
Dominic Ferris <ferris.dominic@gmail.com>
2020-05-21 23:44:40 -0500

    add nav options to left side, fade top nav out when at 40vh

--- dynamic_nav.js
+++ dynamic_nav.js
@@ -1,10 +1,13 @@
 window.addEventListener("scroll", (event) => {
   console.log(scrollY);
   console.log(window.innerHeight);
+
+  // MAKE IT FADE IN/OUT AS PAGE SCROLLS USING OPACITY
+  // const range = window.innerHeight;
 });
 
 $(window).scroll(function () {
-  if ($(this).scrollTop() > window.innerHeight * 0.6) {
+  if ($(this).scrollTop() > window.innerHeight * 0.4) {
     $("header div").fadeOut();
   } else {
     $("header div").fadeIn();
