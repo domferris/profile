8147788548180d7e533b6105f903647ca7e5378a
Dominic Ferris <ferris.dominic@gmail.com>
2020-05-23 16:31:18 -0500

    delete github_scrape.js, functionality in backend

--- javascript/github_scrape.js
+++
@@ -1,6 +0,0 @@
-// CONTRIBUTIONS NUMBER RETRIEVAL VIA GITHUB SCRAPING
-// https://github.com/domferris
-// <h2 class="f4 text-normal mb-2">
-//   _#_ contributions
-//     in the last year
-// </h2>
