e938b804ebadfb19849d82a71e0e885ba9d7129f
Dominic Ferris <ferris.dominic@gmail.com>
2020-05-24 02:26:40 -0500

    remove old header fade in/out, now in scroll event listener with side nav

--- javascript/dynamic_nav.js
+++ javascript/dynamic_nav.js
@@ -1,34 +1,36 @@
-// FADES OUT/IN LANDING NAV
-$(window).scroll(function () {
-  if ($(this).scrollTop() > window.innerHeight * 0.375) {
-    $("header div").fadeOut();
-  } else {
-    $("header div").fadeIn();
-  }
+window.addEventListener("scroll", (event) => {
+  console.log(scrollY);
 });
 
 // SHOW/HIDE SIDE NAV
 window.addEventListener("scroll", (event) => {
+  const landing = document.querySelector("header");
   const sideNav = document.querySelector(".side-nav");
-  // console.log(event.currentTarget.innerHeight);
+
   if (scrollY < event.currentTarget.innerHeight * 0.375) {
     sideNav.classList.remove("active");
+    landing.classList.add("active");
   } else {
+    landing.classList.remove("active");
     sideNav.classList.add("active");
   }
 });
 
+// TODO: REVEAL BOT NAV IF LOADED IN MOBILE BELOW TRANSITION POINT
 window.addEventListener("DOMContentLoaded", (event) => {
   // const sideNav = document.querySelector(".side-nav");
   // const botNav = document.querySelector(".bottom-nav");
 
   if (window.innerWidth <= 600) {
     console.log("mobile loaded!");
+    //if window.scrollY is greater than window.innerHeight * 0.375, botNav has active
   } else {
     console.log(">= tablet loaded!");
+    //if window.scrollY is less than window.innerHeight * 0.375, botNav does not have active
   }
 });
 
+// TODO: TRANSITION FROM SIDE NAV TO BOT NAV WHEN RESIZED TO MOBILE
 window.onresize = () => {
   const sideNav = document.querySelector(".side-nav");
   const botNav = document.querySelector(".bottom-nav");
