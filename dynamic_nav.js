879ab0b8fbe2bd1503ddc6e8bba292e4ee494cc6
Dominic Ferris <ferris.dominic@gmail.com>
2020-05-23 20:22:17 -0500

    add topFunction to "Back to top" alt nav item

--- dynamic_nav.js
+++ dynamic_nav.js
@@ -15,3 +15,9 @@ $(window).scroll(function () {
     $(".alt-nav").fadeOut();
   }
 });
+
+const topFunction = () => {
+  console.log("hi");
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
