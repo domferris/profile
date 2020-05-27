78ebb5574f5359fe681a018fdf64188790f6c7eb
Dominic Ferris <ferris.dominic@gmail.com>
2020-05-27 16:41:46 -0500

    clean up and document code

--- javascript/dynamic_nav.js
+++ javascript/dynamic_nav.js
@@ -1,6 +1,7 @@
-window.addEventListener("scroll", (event) => {
-  console.log(scrollY);
-});
+// // TRACK WINDOW SCROLL
+// window.addEventListener("scroll", (event) => {
+//   console.log(scrollY);
+// });
 
 // SHOW/HIDE ALT NAVS
 window.addEventListener("scroll", (event) => {
@@ -9,44 +10,20 @@ window.addEventListener("scroll", (event) => {
   const botNav = document.querySelector(".bottom-nav");
 
   if (scrollY < event.currentTarget.innerHeight * 0.375) {
-    sideNav.classList.remove("active");
-    botNav.classList.remove("active");
+    // sideNav.classList.remove("active");
+    // botNav.classList.remove("active");
     landing.classList.add("active");
   } else {
     landing.classList.remove("active");
+    // sideNav.classList.add("active");
+    // botNav.classList.add("active");
+  }
+
+  if (landing.classList.contains("active")) {
+    sideNav.classList.remove("active");
+    botNav.classList.remove("active");
+  } else {
     sideNav.classList.add("active");
     botNav.classList.add("active");
   }
 });
-
-// TODO: REVEAL BOT NAV IF LOADED IN MOBILE BELOW TRANSITION POINT
-window.addEventListener("DOMContentLoaded", (event) => {
-  // const sideNav = document.querySelector(".side-nav");
-  // const botNav = document.querySelector(".bottom-nav");
-
-  if (window.innerWidth <= 600) {
-    console.log("mobile loaded!");
-    //if window.scrollY is greater than window.innerHeight * 0.375, botNav has active
-  } else {
-    console.log(">= tablet loaded!");
-    //if window.scrollY is less than window.innerHeight * 0.375, botNav does not have active
-  }
-});
-
-// TODO: TRANSITION FROM SIDE NAV TO BOT NAV WHEN RESIZED TO MOBILE
-// window.onresize = () => {
-//   const sideNav = document.querySelector(".side-nav");
-//   const botNav = document.querySelector(".bottom-nav");
-
-//   if (window.innerWidth <= 600 && window.scrollY > window.innerHeight * 0.375) {
-//     console.log("mobile hello");
-
-//     sideNav.classList.remove("active");
-//     botNav.classList.add("active");
-//   } else {
-//     console.log("mobile goodbye");
-
-//     sideNav.classList.add("active");
-//     botNav.classList.remove("active");
-//   }
-// };
