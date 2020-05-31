582f9c20e2d66efc49d885b9c632da4f65e879ca
Dominic Ferris <ferris.dominic@gmail.com>
2020-05-31 14:31:53 -0500

    add hamburger icon, menu now closes if item is clicked or user scrolls

--- javascript/mobile_nav.js
+++ javascript/mobile_nav.js
@@ -1,9 +1,30 @@
 // EXPAND/COLLAPSE MOBILE NAV
+const mobileNavToggleBtn = document.querySelector(".mobile-toggle");
 const mobileNav = document.querySelector(".mobile-nav");
-const navToggleBtn = document.querySelector(".mobile-nav i");
-// console.log(mobileNav);
-// console.log(navToggleBtn);
 
-navToggleBtn.addEventListener("click", (event) => {
+mobileNavToggleBtn.addEventListener("click", (event) => {
   mobileNav.classList.toggle("open");
+});
+
+// WHEN MOBILE NAV LINK IS CLICKED OR USER SCROLLS, CLOSE MOBILE NAV
+const mobileNavLinks = document.querySelectorAll('nav.mobile-nav a[href*="#"]');
+
+mobileNavLinks.forEach((navLink) => {
+  navLink.addEventListener("click", (event) => {
+    mobileNav.classList.remove("open");
+
+    $("html,body").animate(
+      {
+        scrollTop: $(navLink.getAttribute("href")).offset().top,
+      },
+      500
+    );
+  });
+});
+
+// IF USER SCROLLS WHILE MOBILE NAV IS OPEN, CLOSE MOBILE NAV
+window.addEventListener("scroll", (event) => {
+  if (mobileNav.classList.contains("open")) {
+    mobileNav.classList.remove("open");
+  }
 });
