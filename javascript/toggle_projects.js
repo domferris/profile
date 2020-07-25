079e294bc6787d03fa2a3909334863634ee30707
Dominic Ferris <ferris.dominic@gmail.com>
2020-07-24 19:14:10 -0500

    comment out unused WIP code

--- javascript/toggle_projects.js
+++ javascript/toggle_projects.js
@@ -1,28 +1,28 @@
 // TODO: PROJECTS LIST TOGGLE
-projectsList = document.querySelectorAll(".projects-list");
-projectsSection = document.querySelectorAll(".projects-section");
+// projectsList = document.querySelectorAll(".projects-list");
+// projectsSection = document.querySelectorAll(".projects-section");
 
-const projectsToggle = (event) => {
-  const devProjects = document.querySelector(".development .projects-section");
-  const filmProjects = document.querySelector(".film .projects-section");
-  const photoProjects = document.querySelector(".photo .projects-section");
+// const projectsToggle = (event) => {
+//   const devProjects = document.querySelector(".development .projects-section");
+//   const filmProjects = document.querySelector(".film .projects-section");
+//   const photoProjects = document.querySelector(".photo .projects-section");
 
-  if (event.currentTarget.innerText === "Development") {
-    devProjects.classList.toggle("hidden");
-  }
+//   if (event.currentTarget.innerText === "Development") {
+//     devProjects.classList.toggle("hidden");
+//   }
 
-  if (event.currentTarget.innerText === "Film & Video") {
-    filmProjects.classList.toggle("hidden");
-  }
+//   if (event.currentTarget.innerText === "Film & Video") {
+//     filmProjects.classList.toggle("hidden");
+//   }
 
-  if (event.currentTarget.innerText === "Photography") {
-    photoProjects.classList.toggle("hidden");
-  }
-};
+//   if (event.currentTarget.innerText === "Photography") {
+//     photoProjects.classList.toggle("hidden");
+//   }
+// };
 
-projectsList.forEach((project) =>
-  project.addEventListener("click", projectsToggle)
-);
+// projectsList.forEach((project) =>
+//   project.addEventListener("click", projectsToggle)
+// );
 
 ////////////////////////////////////////
 // CODE BELOW FOR MASS PROJECT SWITCHING
