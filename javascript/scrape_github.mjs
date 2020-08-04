bc93d5e9b298d39507886264e464d0792a398e6f
Dominic Ferris <ferris.dominic@gmail.com>
2020-08-03 19:23:22 -0500

    remove formatNumber now that backend handles number retrieval differently

--- javascript/scrape_github.mjs
+++ javascript/scrape_github.mjs
@@ -1,23 +1,21 @@
-import formatNumber from "./utility/format_number.js";
-
 // CONTRIBUTIONS NUMBER RETRIEVAL VIA GITHUB SCRAPE
 const urlGitHub = window.location.href;
 let backendGitHub;
-if (urlGitHub.includes("localhost") || urlGitHub.includes("127.0.0.1")) {
-  backendGitHub = "http://localhost:3000/scrape_github";
+if (urlGitHub.includes('localhost') || urlGitHub.includes('127.0.0.1')) {
+  backendGitHub = 'http://localhost:3000/scrape_github';
 } else {
-  backendGitHub = "https://domferris.herokuapp.com/scrape_github";
+  backendGitHub = 'https://domferris.herokuapp.com/scrape_github';
 }
 
 fetch(backendGitHub)
   .then((response) => response.json())
   .then((data) => {
-    const contributionsNum = document.querySelector(".contributions-num");
-    contributionsNum.innerHTML = formatNumber(data.contributionsNum);
+    const contributionsNum = document.querySelector('.contributions-num');
+    contributionsNum.innerHTML = data.contributionsNum;
 
     // "loading..." animation fade out upon contributionsNum retrieval
     window.setTimeout(() => {
-      $(".loader").fadeOut(100);
-      $(".loader-wrapper").fadeOut(750);
+      $('.loader').fadeOut(100);
+      $('.loader-wrapper').fadeOut(750);
     }, 500);
   });
--- javascript/utility/format_number.js
+++
@@ -1,6 +0,0 @@
-// properly formats integers with commas
-const formatNumber = (integer) => {
-  return integer.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
-
-export default formatNumber;
