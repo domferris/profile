3075af691bec35ee4119e9998fcb897bd57bff97
Dominic Ferris <ferris.dominic@gmail.com>
2020-08-11 16:59:08 -0500

    add nav switching function, pass into existing event listener

--- javascript/dynamic_nav.js
+++ javascript/dynamic_nav.js
@@ -1,15 +1,15 @@
 // SHOW/HIDE ALT NAVS
-const landingNav = document.querySelector("header");
-const sideNav = document.querySelector(".side-nav");
-const mobileNav = document.querySelector(".mobile-nav");
+const landingNav = document.querySelector('header');
+const sideNav = document.querySelector('.side-nav');
+const mobileNav = document.querySelector('.mobile-nav');
 
 const breakPointPercentage = 0.4;
 const breakPointHeight = window.innerHeight * breakPointPercentage;
 
-const enable = (element) => element.classList.add("active");
-const disable = (element) => element.classList.remove("active");
+const enable = (element) => element.classList.add('active');
+const disable = (element) => element.classList.remove('active');
 
-window.addEventListener("scroll", (event) => {
+const switchNav = (event) => {
   if (scrollY < breakPointHeight) {
     enable(landingNav);
     disable(sideNav);
@@ -19,4 +19,6 @@ window.addEventListener("scroll", (event) => {
     enable(sideNav);
     enable(mobileNav);
   }
-});
+};
+
+window.addEventListener('scroll', switchNav);
